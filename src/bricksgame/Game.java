dc7f7a25c818c2dd1110009894adbd5c60198fcd
christer Holm <ch@star78.se>
2021-09-28 08:38:12 +0200

    Moving ball

--- src/bricksgame/Game.java
+++ src/bricksgame/Game.java
@@ -10,59 +10,96 @@ import java.awt.event.KeyListener;
 public class Game  extends JPanel implements KeyListener, ActionListener {
     private boolean play = false;
     private int score = 0;
-private int totalBricks = 21;
-private Timer timer;
-private int delay = 8;
-private int playerX = 310;
-private int ballposX = 120;
-private int ballposY =350;
-private int ballXdir = -1;
-private int ballYdir = -2;
+    private int totalBricks = 21;
+    private Timer timer;
+    private int delay = 8;
+    private int playerX = 310;
+    private int ballposX = 120;
+    private int ballposY = 350;
+    private int ballXdir = -1;
+    private int ballYdir = -2;
 
-public Game() {
-addKeyListener(this);
-setFocusable(true);
-setFocusTraversalKeysEnabled(false);
-timer = new Timer(delay,this);
-timer.start();
+    public Game() {
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+       timer = new Timer(delay, this);
+       timer.start();
 
-}
-public void paint(Graphics graphics){
-    graphics.setColor(Color.cyan);
-    graphics.fillRect(1,1,692,592);
+    }
+
+    public void paint(Graphics graphics) {
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(1, 1, 692, 592);
 //border
-    graphics.setColor(Color.BLACK);
-    graphics.fillRect(1,1,3,592);
-    graphics.fillRect(1,1,692,3);
-    graphics.fillRect(691,1,3,592);
+        graphics.setColor(Color.YELLOW);
+        graphics.fillRect(0, 0, 3, 592);
+        graphics.fillRect(0, 0, 691, 3);
+        graphics.fillRect(691, 0, 3, 592);
 //paddel
- graphics.setColor(Color.BLUE);
- graphics.fillRect(playerX, 550,100,8);
+        graphics.setColor(Color.BLUE);
+        graphics.fillRect(playerX, 550, 100, 8);
 
 //the Ball
-    graphics.setColor(Color.RED);
-    graphics.fillRect(ballposX, ballposY,20,20);
-}
-
+        graphics.setColor(Color.RED);
+        graphics.fillOval(ballposX, ballposY, 20, 20);
+   graphics.dispose();
+    }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
+timer.start();
+if(play){
+    ballposX+=ballXdir;
+    ballposY+=ballYdir;
 
+    if(ballposX < 0){
+        ballXdir = -ballXdir;
+    }
+    if(ballposY < 0){
+        ballYdir = -ballYdir;
+}
+    if(ballposX > 670) {
+        ballXdir = -ballXdir;
+    }
+}
+
+repaint();
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {
+    public void keyTyped(KeyEvent e) {}
 
-    }
+    @Override
+    public void keyReleased(KeyEvent e) {}
 
     @Override
     public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (playerX >= 600) {
+                playerX = 600;
+            } else {
+                moveRight();
+            }
 
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (playerX < 10) {
+                playerX = 10;
+            } else {
+                moveLeft();
+            }
+        }
     }
 
-    @Override
-    public void keyReleased(KeyEvent e) {
+    private void moveRight() {
+        play = true;
+        playerX+=20;
 
+    }
+    private void moveLeft() {
+        play = true;
+        playerX-=20;
     }
 }
